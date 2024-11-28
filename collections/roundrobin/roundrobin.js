4349617eb6a6b6c9039385bb2e4f185fe7b5fbfe
ay <shlemafona@gmail.com>
2024-11-28 09:52:04 +0200

    roundrobin: improve

--- collections/roundrobin/roundrobin.js
+++ collections/roundrobin/roundrobin.js
@@ -64,18 +64,4 @@ class RoundRobin {
   }
 }
 
-const factory = () => ({
-  instance: factory.counter ? ++factory.counter : (factory.counter = 1),
-});
-
-const main = async () => {
-  const collection = new RoundRobin(factory, { size: 3 });
-  const promises = [];
-  for (let i = 0; i < 5; i++) {
-    const instance = collection.getInstance();
-    promises.push(instance);
-  }
-  Promise.all(promises);
-};
-
-main();
+module.exports = {RoundRobin}
