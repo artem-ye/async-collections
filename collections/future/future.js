4168efd9c03f07d5cb379134ffa566db7f7b2f55
ay <shelmafona@gmail.com>
2024-12-03 12:51:07 +0200

    future: complete function based realization

--- collections/future/future.js
+++ collections/future/future.js
@@ -3,11 +3,18 @@
 const future = (executor) => ({
   fork(cb) {
     executor(cb);
-    return this;
+  },
+  chain(fn) {
+    return future((resolve) => this.fork((v) => fn(v).fork(resolve)));
+  },
+  map(fn) {
+    return this.chain((v) => future.of(fn(v)));
   },
 });
-
 future.of = (value) => future((cb) => cb(value));
 
-future((r) => r(5)).fork(console.log);
-// future.of(5).fork((resolve) => console.log(resolve));
+future
+  .of(5)
+  .map((v) => v + 2)
+  .map((v) => v + 3)
+  .fork(console.log);
